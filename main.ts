3810c0360aa02a18720f5d78ec5c721a4b69e15e
svi-berlin <svi-berlin@users.noreply.github.com>
2024-06-18 13:41:47 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    BravZustand = 80
+    BravStart = input.runningTime()
+    pins.servoWritePin(AnalogPin.C17, BravZustand)
+})
+let BravStart = 0
+let BravZustand = 0
+BravZustand = 80
+let Testzeit = 5000
+let Laermgrenze = 585
+BravStart = input.runningTime()
+let BravDauer = 0
+let BravLimit = 30000
+pins.servoWritePin(AnalogPin.C17, BravZustand)
+basic.forever(function () {
+	
+})
